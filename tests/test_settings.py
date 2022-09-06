43391f03c0154e2f5b61a5a04d95e53fd5566399
Jose Vazquez <josevazjim88@gmail.com>
2022-09-06 16:01:58 +0200

    adding

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -9,7 +9,8 @@ def test_create_settings_passing_params_redis():
 
     redis_settings = RedisSettings(redis_password="pass")
     settings = Settings(pu=redis_settings)
-    assert issubclass(settings.backend(), RedisLog)
+    klass = settings.backend()
+    assert issubclass(klass, RedisLog)
 
 
 def test_create_settings_passing_params_mongo():
@@ -20,7 +21,8 @@ def test_create_settings_passing_params_mongo():
         mongo_collection="logs",
     )
     settings = Settings(pu=mongo_settings)
-    assert issubclass(settings.backend(), MongoLog)
+    klass = settings.backend()
+    assert issubclass(klass, MongoLog)
 
 
 def test_create_settings_passing_params_sql():
@@ -30,7 +32,8 @@ def test_create_settings_passing_params_sql():
         sql_table="logs",
     )
     settings = Settings(pu=sql_settings)
-    assert issubclass(settings.backend(), SQLLog)
+    klass = settings.backend()
+    assert issubclass(klass, SQLLog)
 
 
 # env vars
@@ -38,14 +41,14 @@ def test_create_settings_env_vars_redis(env_vars_redis):
     settings = Settings()
     klass = settings.backend()
     assert klass == RedisLog
-    assert klass.Meta.redis_password == "pass"
+    assert klass.Meta.redis_password == RedisLog.Meta.redis_password == "pass"
 
 
 def test_create_settings_env_vars_mongo(env_vars_mongo):
     settings = Settings()
     klass = settings.backend()
     assert klass == MongoLog
-    assert klass.Meta.mongo_connection == "mongodb://user:pass@mongo:27017"
+    assert klass.Meta.mongo_connection == MongoLog.Meta.mongo_connection == "mongodb://user:pass@mongo:27017"
 
 
 def test_create_settings_env_vars_sql(env_vars_sql):
@@ -53,6 +56,6 @@ def test_create_settings_env_vars_sql(env_vars_sql):
     klass = settings.backend()
     assert klass == SQLLog
     assert (
-        klass.Meta.sql_dsn
+        klass.Meta.sql_dsn == SQLLog.Meta.sql_dsn
         == "postgresql+psycopg2://user:pass@postgres:5432/db"
     )
