7114807d930ef66a4ed8348466dfc3f46370262d
Jose Vazquez <josevazjim88@gmail.com>
2022-09-06 16:22:57 +0200

    adding

---
+++ tests/test_updater/test_utils.py
@@ -0,0 +1,80 @@
+from updater.utils import progress_updater
+from updater.backends import Settings
+from updater.backends.sql import SQLSettings, SQLLog
+from updater.backends.mongo import MongoSettings, MongoLog
+from updater.backends.redis import RedisSettings, RedisLog
+
+
+def test_progress_updater_passing_params_redis(redis):
+    redis_settings = RedisSettings(redis_password="pass")
+
+    @progress_updater(task_name="My task", settings=redis_settings)
+    def task():
+        return "doing a lot of work..."
+
+    task()
+
+    assert redis.get()
+
+
+def test_progress_updater_passing_params_mongo():
+
+    mongo_settings = MongoSettings(
+        mongo_connection="mongodb://user:pass@mongo:27017",
+        mongo_db="db",
+        mongo_collection="logs",
+    )
+    settings = Settings(pu=mongo_settings)
+    klass = settings.backend()
+    assert issubclass(klass, MongoLog)
+    assert (
+        klass.Meta.mongo_connection
+        == MongoLog.Meta.mongo_connection
+        == "mongodb://user:pass@mongo:27017"
+    )
+
+
+def test_progress_updater_passing_params_sql():
+
+    sql_settings = SQLSettings(
+        sql_dsn="postgresql+psycopg2://user:pass@postgres:5432/db",
+        sql_table="logs",
+    )
+    settings = Settings(pu=sql_settings)
+    klass = settings.backend()
+    assert issubclass(klass, SQLLog)
+    assert (
+        klass.Meta.sql_dsn
+        == SQLLog.Meta.sql_dsn
+        == "postgresql+psycopg2://user:pass@postgres:5432/db"
+    )
+
+
+# env vars
+def test_progress_updater_env_vars_redis(env_vars_redis):
+    settings = Settings()
+    klass = settings.backend()
+    assert klass == RedisLog
+    assert klass.Meta.redis_password == RedisLog.Meta.redis_password == "pass"
+
+
+def test_progress_updater_env_vars_mongo(env_vars_mongo):
+    settings = Settings()
+    klass = settings.backend()
+    assert klass == MongoLog
+    assert (
+        klass.Meta.mongo_connection
+        == MongoLog.Meta.mongo_connection
+        == "mongodb://user:pass@mongo:27017"
+    )
+
+
+def test_progress_updater_env_vars_sql(env_vars_sql):
+    settings = Settings()
+    klass = settings.backend()
+    assert klass == SQLLog
+    assert (
+        klass.Meta.sql_dsn
+        == SQLLog.Meta.sql_dsn
+        == "postgresql+psycopg2://user:pass@postgres:5432/db"
+    )
